7631255dad7b04467efde890ef0e3940a36a776f
Kirsten Frager <kirstenfrager@kirstens-air.ait-sydney.net.au>
2017-05-31 10:41:25 +1000

    passport google-auth implemented

--- config/auth.js
+++ config/auth.js
@@ -3,5 +3,11 @@ module.exports = {
     'FACEBOOK_APP_ID': process.env.FACEBOOK_APP_ID,
     'FACEBOOK_APP_SECRET': process.env.FACEBOOK_APP_SECRET,
     'FB_CALLBACK_URL': process.env.FB_CALLBACK_URL,
+  },
+  
+  'googleAuth': {
+    'GOOGLE_APP_ID': process.env.GOOGLE_APP_ID,
+    'GOOGLE_APP_SECRET': process.env.GOOGLE_APP_SECRET,
+    'GOOGLE_CALLBACK_URL': process.env.GOOGLE_CALLBACK_URL,
   }
 }
