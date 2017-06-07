8b95868beace69441f31f1707121ef3e2bc2fcd7
Kirsten Frager <kirstenfrager@kirstens-air.ait-sydney.net.au>
2017-06-07 16:10:49 +1000

    test refactor

--- test/calendar.test.js
+++ test/calendar.test.js
@@ -1,44 +1,4 @@
 const expect = require('chai').expect;
-//
-// function getTimeSlots(today, numDays) {
-//
-//   let times = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-//   let slots = [];
-//
-//   for (let i = 0; i < numDays; i++) {
-//
-//     let dailySlots = times.map((time) => {
-//       let date = new Date(today.getTime());
-//       return {free: true, startTime: time, endTime: time+1, date: new Date(date.setDate(date.getDate()+i))}
-//     })
-// console.log(dailySlots)
-//     slots = slots.concat(dailySlots)
-//   }
-//
-//   return slots;
-// }
-
-// const expect = require('chai').expect;
-// const getTimeSlots = require('../models/bookingSlots');
-
-// function getTimeSlots(today, numDays) {
-//
-//   let times = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-//   let slots = [];
-//
-//   for (let i = 0; i < numDays; i++) {
-//
-//     let dailySlots = times.map((time) => {
-//       let date = new Date(today.getTime());
-//       return {free: true, startTime: time, endTime: time+1, date: new Date(date.setDate(date.getDate()+i))}
-//     })
-// console.log(dailySlots)
-//     slots = slots.concat(dailySlots)
-//   }
-//
-//   return slots;
-// }
-
 
 describe('calendar tests', function() {
 
